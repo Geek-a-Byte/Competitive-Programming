4e2d5f7234915ee2bcf9889412b5d925c8de2f3b
Nazia Shehnaz Joynab <ncoder76@gmail.com>
2022-03-19 13:05:08 +0600

    Create discovering_permutations_3.cpp

---
+++ Recursion and Backtracking/medium/discovering_permutations_3.cpp
@@ -0,0 +1,49 @@
+// approach 3 -> passing nums by reference
+#include<bits/stdc++.h>
+using namespace std;
+
+int k;
+vector<int>nums;
+void permutations(int ind,int n,vector<int>& nums)
+{
+    if(k==0) return;
+    if(ind==n)
+    {
+        k--;
+        for(int i=0;i<nums.size();i++)
+        {
+            printf("%c", nums[i]+65);
+        }
+        cout<<endl;
+        return;
+    }
+    else
+    {
+        for(int i=ind;i<n;i++)
+        {
+            swap(nums[ind],nums[i]);
+            permutations(ind+1,n,nums);
+        }
+        for(int i = n - 1; i > ind; i--)         // Dont need to do this if passing nums by value.
+            swap(nums[i], nums[ind]);
+    }
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cout<<"Case "<<i<<":"<<endl;
+        int n;
+        cin>>n>>k;
+        nums.clear();
+        for(int i=0;i<n;i++)
+        {
+            nums.push_back(i);
+        }
+        permutations(0,n,nums);
+       
+    }
+}
